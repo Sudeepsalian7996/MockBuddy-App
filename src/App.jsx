80f47bfa4690080b021c1074799f7543226da486
spo <sp@decodeup.email>
2024-06-25 23:14:57 +0530

    created folder structure and added basic styled component styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import viteLogo from "/vite.svg";
 import "./App.css";
+import styled from "styled-components";
+
+export const StyledInfo1 = styled.div`
+  background: ${({ theme }) => theme.colors.dark};
+  padding: 1rem;
+  margin: 1rem 0;
+  color: orange;
+  font-family: ${({ theme }) => theme.fonts.body};
+`;
 
 function App() {
-  return <>hlo</>;
+  return <StyledInfo1>Name is SUDEEP</StyledInfo1>;
 }
 
 export default App;
