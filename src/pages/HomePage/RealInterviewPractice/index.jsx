cf1c0db4b7de0258af4a21bbdfa0a08ddbf89e0c
spo <sp@decodeup.email>
2024-08-16 22:23:09 +0530

    Real interview section added

--- src/pages/HomePage/RealInterviewPractice/index.jsx
+++ src/pages/HomePage/RealInterviewPractice/index.jsx
@@ -1,23 +1,18 @@
-import { Box, CardContent, Typography, Container } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import React from "react";
-import whoCanUseImg from "assets/images/whoCanUseImg.webp";
+import realInterviewPractice from "assets/images/realInterviewPractice.png";
 
-const WhoCanUseSection = () => {
+const RealInterviewPractice = () => {
   return (
     <Container
       sx={{
         display: "flex",
+        alignItems: "center",
         gap: "2rem",
         marginTop: { xs: "4rem", md: "7rem" },
         flexDirection: { xs: "column", md: "row" },
       }}
     >
-      <Box
-        component="img"
-        src={whoCanUseImg}
-        alt="Who can use Section"
-        sx={{ width: { xs: "100%", md: "50%" } }}
-      ></Box>
       <Box>
         <Typography
           sx={{
@@ -27,7 +22,7 @@ const WhoCanUseSection = () => {
             marginBottom: "1rem",
           }}
         >
-          Who's using it
+          Need real time practice?
         </Typography>
         <Typography
           sx={{
@@ -37,18 +32,25 @@ const WhoCanUseSection = () => {
             marginBottom: "1rem",
           }}
         >
-          How everyone in tech prepares
+          Get real interview practice
         </Typography>
         <Typography
           sx={{ fontSize: "1.1rem", color: "#4B5563", lineHeight: "1.8rem" }}
         >
-          Exponent Practice supports interview prep for everyone in tech. From
-          product management to software engineering and data roles, there are
-          thousands of practice questions to choose from.
+          Exponent Practice aims to provide the complete tech interview practice
+          you need, for free. Hone your interviewing skills, learn from your
+          peers, and become comfortable performing under pressure within an
+          interview setting.
         </Typography>
       </Box>
+      <Box
+        component="img"
+        src={realInterviewPractice}
+        alt="Who can use Section"
+        sx={{ width: { xs: "100%", md: "50%" } }}
+      ></Box>
     </Container>
   );
 };
 
-export default WhoCanUseSection;
+export default RealInterviewPractice;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,11 +4,13 @@ import FeedbackSection from "pages/HomePage/FeedbackSection/FeedbackSection";
 import PracticeInfoSection from "pages/HomePage/PracticeInfoSection";
 import WhyPracticeSection from "pages/HomePage/WhyPracticeSection";
 import WhoCanUseSection from "pages/HomePage/WhoCanUseSection";
+import RealInterviewPractice from "pages/HomePage/RealInterviewPractice";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <RealInterviewPractice />
       <WhoCanUseSection />
       <PracticeInfoSection />
       <WhyPracticeSection />
